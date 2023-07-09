90889df69f0f2b8be448a19f1af563976493674f
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-07-09 14:06:06 +0200

    handle ajax requests

--- tweets/views.py
+++ tweets/views.py
@@ -16,11 +16,14 @@ def home_view(request, *args, **kwargs):
     return render(request, 'tweets/home.html', context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    is_ajax = bool(request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest')
     form = TweetsForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
         obj=form.save(commit=False)
         obj.save()
+        if is_ajax:
+            return JsonResponse({}, status=201) # 201 == created items
         if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetsForm()
