b3135a6cd7dcd6a10a82fa04b872b6feeb7a8b38
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-07-11 20:47:22 +0200

    create tweets model admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import TweetsModel
+
 # Register your models here.
+admin.site.register(TweetsModel)
--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 import random
 
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class TweetsModel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
     
