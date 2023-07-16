61ba1059848a2eb0b0a6f62e4a695be1dddd2166
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-07-16 15:59:41 +0200

    create tweetlike model

--- tweets/models.py
+++ tweets/models.py
@@ -5,11 +5,17 @@ from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
-# Create your models here.
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("TweetsModel", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class TweetsModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name = 'tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     
     
     class Meta:
