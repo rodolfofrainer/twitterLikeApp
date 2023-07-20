9f649bad95eb82d40974d0ced20cb5521781e673
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-07-20 19:06:15 +0200

    update tweetActionSerializer

--- tweets/views.py
+++ tweets/views.py
@@ -63,6 +63,7 @@ def tweet_action_view(request, *args, **kwargs):
         data = serializer.validated_data
         tweet_id = data.get('id')
         action = data.get('action')
+        content = data.get('content')
         qs = TweetsModel.objects.filter(id=tweet_id)
         if not qs.exists():
             return Response({}, status=404)
@@ -74,7 +75,13 @@ def tweet_action_view(request, *args, **kwargs):
         elif action == 'unlike':
             obj.likes.remove(request.user)
         elif action == 'retweet':
-            pass
+            new_tweet = TweetsModel.objects.create(
+                user=request.user,
+                parent=obj,
+                content=content,
+                )
+            serializer = TweetSerializer(new_tweet)
+            return Response(serializer.data, status=200)
     return Response({}, status=200)
 
 
