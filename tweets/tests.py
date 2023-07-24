43c66f5e33b532888ee977aaace65d07e20e0abc
Rodolfo Frainer <rodolfo_frainer@hotmail.com>
2023-07-24 22:50:35 +0200

    create react app

--- tweets/tests.py
+++ tweets/tests.py
@@ -10,31 +10,33 @@ User = get_user_model()
 class TweetTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(username='some_random_user', password='some_random_password')
+        print("User created:", self.user)
         TweetsModel.objects.create(content='my first tweet', user=self.user)
         TweetsModel.objects.create(content='my first tweet', user=self.user)
         TweetsModel.objects.create(content='my first tweet', user=self.user)
         
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password=self.user.password)
+        print("Client session:", client.session)
+        return client
 
     def test_tweet_created(self):
         tweet_obj = TweetsModel.objects.create(content='my second tweet', user=self.user)
         self.assertEqual(tweet_obj.id, 4)
         self.assertEqual(tweet_obj.user, self.user)
 
-    def get_client(self):
-        client = APIClient()
-        client.login(username=self.user.username, password='some_random_password')
-        return client
 
     def test_tweet_list(self):
         client = self.get_client()
-        url = reverse('tweets:tweets_list')
+        url = '/api/tweets/'
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 3)
         
     def test_action_like(self):
         client = self.get_client()
-        url = reverse('tweets:tweet_action')
+        url = '/api/tweets/action/'
         data = {'id': 1, 'action': 'like'}
         response = client.post(url, data)
         self.assertEqual(response.status_code, 200)
--- tweets/views.py
+++ tweets/views.py
@@ -76,6 +76,8 @@ def tweet_action_view(request, *args, **kwargs):
             return Response(serializer.data, status=200)
         elif action == 'unlike':
             obj.likes.remove(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == 'retweet':
             new_tweet = TweetsModel.objects.create(
                 user=request.user,
@@ -83,7 +85,7 @@ def tweet_action_view(request, *args, **kwargs):
                 content=content,
                 )
             serializer = TweetSerializer(new_tweet)
-            return Response(serializer.data, status=200)
+            return Response(serializer.data, status=201)
     return Response({}, status=200)
 
 
